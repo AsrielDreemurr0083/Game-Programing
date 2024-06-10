bd4b6178d65a44a9c4279e608501dc2a8308b662
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-06-10 15:45:42 +0900

    (2024.6.10) 단일 연결 리스트

---
+++ Program/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Program
+{
+    public class SingleLinkedList<T>
+    {       
+        private class Node
+        {
+            public T data;
+            public Node next;
+        }
+
+        private Node head;
+        private int size;
+
+        public SingleLinkedList()
+        {
+            size = 0;
+            head = null;
+        }
+
+        public void PushFront(T data)
+        {
+            if(head == null)
+            {
+                head = new Node();
+
+                head.data = data;
+                head.next = null;
+            }
+            else
+            {
+                Node newNode = new Node();
+                newNode.data = data;
+                newNode.next = head;
+
+                head = newNode;
+            }
+
+            size++;
+
+        }
+
+        public void Show()
+        {
+            Node currentNode = head;
+
+            while(currentNode != null)
+            {
+                Console.WriteLine(currentNode.data);
+                currentNode = currentNode.next;
+            }
+        }
+
+        public void Size()
+        {
+            //size = 
+        }
+    }
+
+    
+    
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleLinkedList<int> singleLinkedList = new SingleLinkedList<int>();
+
+            singleLinkedList.PushFront(30);
+            singleLinkedList.PushFront(20);
+            singleLinkedList.PushFront(10);
+
+            //Console.Write("singleLinkedList의 size : " + singleLinkedList.Size());
+
+            singleLinkedList.Show();
+        }
+    }
+}
