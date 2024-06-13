4c56a00d1c026aefc23c7b8127ca34c709c67489
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-06-13 15:45:47 +0900

    (2024.6.13) 스택

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,219 +1,54 @@
-﻿namespace Program
-{
-    public class DoubleLinkedList<T>
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+namespace Program
+{   
+    public class CircleLinkedList<T>
     {
-        public class Node
+        private class Node
         {
-            public T data;
-            public Node next;
-            public Node previous;
+           public T data;
+           public Node next;
         }
 
+        private Node head; 
         private int size;
-        private Node head;
-        private Node tail;
-        
-        
-        
 
-
-        public DoubleLinkedList()
+        public CircleLinkedList()
         {
             size = 0;
             head = null;
-            tail = null;
         }
 
         public void PushBack(T data)
         {
             Node newNode = new Node();
-
-            if(tail == null)
-            {
-                head = newNode;
-                tail = newNode;
-
-                newNode.data = data;
-                newNode.next = null;
-                newNode.previous = null;
-            }
-            else
-            {
-                tail.next = newNode;
-                newNode.previous = tail;
-
-                tail = newNode;
-
-                newNode.data = data;
-                newNode.next = null;
-            }
-            size++;
-        }
-
-        public void PushFront(T data)
-        {
-            //Node newNode = new Node();
-            //newNode.data = data;
-            //
-            //if (head == null)
-            //{
-            //    head = newNode;
-            //    tail = newNode;
-            //
-            //    newNode.next = null;
-            //    newNode.previous = null;
-            //}
-            //else
-            //{
-            //    newNode.next = head;
-            //    head.previous = newNode;
-            //
-            //    head = newNode;
-            //    newNode.previous = null;
-            //}
-            //size++;
-
-            if(head == null)
-            {
-                head = new Node();
-                tail = head;
-
-                head.data = data;
-                head.next = null;
-                head.previous = null;
-            }
-            else
-            {
-                Node newNode = new Node();
-
-                newNode.next = head;
-
-                head.previous = newNode;
-                head = newNode;
-
-                newNode.data = data;
-                newNode.previous = null;
-            }
-            size++;
-        }
-
-        public int Size()
-        {
-            return size;
-        }
-
-        public void RemoveBack()
-        {           
-            if (tail == null)
-            {
-                Console.WriteLine("Linked List is Empty");
-            }
-            else
-            {
-                if (head == tail)
-                {                    
-                    tail = tail.next;
-                    head = tail;
-                }
-                else
-                {
-                    tail.previous.next = null;
-                    tail = tail.previous;
-                }
-                size--;
-                
-            }
-        }
-
-        public void RemoveFront()
-        {
-            //if (head == null)
-            //{
-            //    Console.WriteLine("LinkedList is Empty");
-            //}
-            //else
-            //{
-            //    if(head == tail)
-            //    {
-            //        head = head.previous;
-            //        tail = head;
-            //    }
-            //    else
-            //    {
-            //        head.next.previous = null;
-            //        head = head.next;
-            //    }
-            //    size--;
-            //}
-
-            //정답
+            newNode.data = data;
             if (head == null)
             {
-                Console.WriteLine("Linked List is Empty");
+                head = newNode;
+
+                newNode.next = head;
             }
             else
             {
-                if(head == tail)
-                {
-                    head = head.previous;
-                    tail = head;
-                }
-                else
-                {
-                    head.next.previous = null;
-                    head = head.next;
-                }
-                size--;
+                newNode.next = head.next;
+                head.next = newNode;
+                head = newNode;
             }
+            
         }
-
-        public Node First()
-        {
-            return head;
-        }
-        public Node Last()
-        {
-            return tail;
-        }
-
-        void AddAfter(Node position, T data)
-        {
-            if(head.next == null)
-            {
-                head.next = position;
-            }
-        }
-
-        public void Show()
-        {
-            Node currentNode = head;
-
-            while (currentNode != null)
-            {
-                Console.WriteLine(currentNode.data);
-                currentNode = currentNode.next;
-            }
-        }
-
+        
     }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            DoubleLinkedList<int> doubleLinkedList = new DoubleLinkedList<int> ();
-            
-            
+            CircleLinkedList<int> circleLinkedList = new CircleLinkedList<int>();
 
-            doubleLinkedList.PushBack(10);            
-            doubleLinkedList.PushBack(20);            
-            doubleLinkedList.PushBack(30);
-            doubleLinkedList.PushBack(40);
-
-            Console.WriteLine("DoubleLinkedList의 size : " + doubleLinkedList.Size());
-            
-            doubleLinkedList.Show();
-
+            circleLinkedList.PushBack(10);
+            circleLinkedList.PushBack(20);
+            circleLinkedList.PushBack(30);
 
 
         }
