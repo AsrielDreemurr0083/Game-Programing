c9d5fba879c9fe478561c689607b07e3280abe55
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-06-20 14:21:42 +0900

    (2024.6.20) 파일 입출력

---
+++ Class9th (File Input & Output)/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Class9th__File_Input___Output_
+{
+    internal class Program
+    {
+        static void LoadText(string fileName)
+        {
+            StreamReader streamReader1 = new StreamReader("image1.txt");
+            while (streamReader1.Peek() >= 0)
+            {
+                Console.WriteLine(streamReader1.ReadLine());
+            }
+            StreamReader streamReader2 = new StreamReader("images2.txt");
+            while (streamReader2.Peek() >= 0)
+            {
+                Console.WriteLine(streamReader2.ReadLine());
+            }
+            StreamReader streamReader3 = new StreamReader("images3.txt");
+            while (streamReader3.Peek() >= 0)
+            {
+                Console.WriteLine(streamReader3.ReadLine());
+            }
+            StreamReader streamReader4 = new StreamReader("images4.txt");
+            while (streamReader4.Peek() >= 0)
+            {
+                Console.WriteLine(streamReader4.ReadLine());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // File : 파일에 대한 생성, 복사, 이동 및 열기를 위한 클래스
+            // FileInfo : 파일에 대한 생성, 복사, 이동 및 열기에 대한 속성
+            // FileStream : 파일에 대한 스트림을 제공하여 동기 및 비동기 읽기/쓰기를 지원           
+
+            //FileStream fileStream = File.Create("data.txt");
+
+            //fileStream.Close();
+
+            // StreamReader : 문자열에서 읽어오는 TextReade 구현한다.
+            // StreamWriter : TextWriter를 구현하여 특정 인코딩을 스트림에 문자로 저장한다.
+
+            //StreamWriter streamWriter = new StreamWriter("data.txt");
+            //
+            //streamWriter.WriteLine("HP 100");
+            //streamWriter.WriteLine("Level 10");
+            //streamWriter.WriteLine("Name Warrior");
+            //
+            //streamWriter.Close();
+
+            //StreamReader streamReader = new StreamReader("images4.txt");
+            //while (streamReader.Peek() >= 0)
+            //{
+            //    Console.WriteLine(streamReader.ReadLine());
+            //}
+            //streamReader.Close();
+
+            LoadText("image1.txt");
+            LoadText("images2.txt");
+            LoadText("images3.txt");
+            LoadText("images4.txt");
+
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -2,100 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Drawing;
+using System.IO;
 namespace Program
-{
-    public class Stack<T>
-    {
-        private int top;
-
-        private readonly int arraySize;
-        private T error;
-        private T [] array;
-
-        public Stack()
-        {
-            top = -1;
-            arraySize = 5;
-
-            array = new T[arraySize];
-        }
-
-        public void Push(T data)
-        {
-            if(top >= arraySize-1)
-            {
-                Console.WriteLine("Stack Overflow");
-            }
-            else
-            {
-                array[++top] = data;
-            }            
-        }
-
-        public T Peek()
-        {
-            return array[++top];
-        }
-
-        public T Pop()
-        {
-            if (top <= -1)
-            {
-                Console.WriteLine("Stack is Empty");
-                return error;
-            }
-            else
-            {
-                return array[--top];
-            }
-        }
-
-        public bool Constains(T data)
-        {
-            for(int i = 0; i <= top; i++) 
-            {
-                if (array[i].ToString() == data.ToString())
-                {
-                    return true;
-                    
-                }
-            }
-            return false;
-        }
-
-    }
+{       
     internal class Program
-    {
-        //static bool CheckBracket(string content)
-        //{
-        //    if(content == null)
-        //    {
-        //        return false;
-        //    }
-        //    Stack<char> stack = new Stack<char>();
-        //}
-
-
-
-            static void Main(string[] args)
+    {        
+        static void Main(string[] args)
         {
-            Stack<int> stack = new Stack<int>();
 
-            stack.Push(10);
-            stack.Push(20);
-            stack.Push(30);
-            stack.Push(40);
-            stack.Push(50);
-           
-
-            Console.WriteLine("Stack의 Constains(30) : " + stack.Constains(99));
-
-            Console.WriteLine("Stack의 Peek : " + stack.Pop());
-            Console.WriteLine("Stack의 Peek : " + stack.Pop());
-            Console.WriteLine("Stack의 Peek : " + stack.Pop());
-            Console.WriteLine("Stack의 Peek : " + stack.Pop());
-           
-            
         }
     }
 }
